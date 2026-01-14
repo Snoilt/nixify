0bc244044a80c197809ef0cb7c96198b32bb62f5
Snoilt <paul@oellers.net>
2026-01-14 13:41:01 +0100

    feat: change username

--- modules/common.nix
+++ modules/common.nix
@@ -49,7 +49,7 @@ in {
   };
 
   config = {
-    my.username = lib.mkDefault "o";
+    my.username = lib.mkDefault "p";
 
     nix = {
       settings.experimental-features = "nix-command flakes";
@@ -68,8 +68,8 @@ in {
             user.signingkey = "${userHome}/.ssh/id_ed25519.pub";
             commit.gpgsign = true;
             tag.gpgsign = true;
-            user.name = "vaaski";
-            user.email = "admin@vaa.ski";
+            user.name = "Snoilt";
+            user.email = "paul@oellers.net";
             pull.rebase = true;
           };
         };
@@ -80,17 +80,7 @@ in {
     users.users.${config.my.username} = {
       shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
-        # atlas
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCn75kBDz7/E92HNrgITF1rGQvoKIL9kZfECCEgNKaCtoTxr10L/TO1Uvq8LZMdxL3HIYO6PGJKzuYIsfEvN7Bx/PG0FXU7QWge0H0j3jCjgWx5p7RPwH76omIIryz0V7Vt+xPFJeGykiW0qmuHl8zK/uxVtHN/cVW+ukmpQ6ztmnRJ9HrGiYIOuOfNgnVr7J6i7lYv8kL+0l7gmBABnMIQk+7cIntgd54jroAdvcPQpja8pO6uki5Eh9XJrAmo/nW9KTjRSx+DtxuQny5lh7jZlwmKZtkIyV6MgoeWopDDWl69BmCzbRDh8GpnkHlCP09WGi6XMFQSGgmOIOhokE+D"
-
-        # i15p
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGYlfzHsYEWnzrrdM4t8GC9uA8SIIvjtieZEmIbw/yNn"
-
-        # work mac
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKoiophA8kvDCGunKUiRX91opLWPoNUi+LIsVv+bCmz2"
-
-        # m2-air
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN1Z1+udAEvwovjH5/0Mqzrg/iC10+A9KMl2rssjglLB"
+        # add m2 air 
       ];
     };
 
@@ -119,7 +109,7 @@ in {
       go
       goreleaser
       gow
-      nerd-fonts.fira-code
+      nerd-fonts.jetbrains-mono
       nixd
       nodejs_24
       pnpm
